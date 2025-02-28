295d5002b1a725a369f61b996b811c09b6a8310c
Kazte <castellittofranco1@gmail.com>
2025-02-28 00:11:25 -0300

    feat: add hasher

---
+++ ualax.api/ualax.application/Abstractions/Authentication/IHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ualax.application.Abstractions.Authentication
+{
+    public interface IHasher
+    {
+        string Hash(string key);
+        string Unhash(string hashedKey);
+    }
+}
---
+++ ualax.api/ualax.infrstructure/Authentication/UsernameHasher.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using ualax.application.Abstractions.Authentication;
+
+namespace ualax.infrastructure.Authentication
+{
+    public class UsernameHasher : IHasher
+    {
+        public string Hash(string key)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(key));
+        }
+
+        public string Unhash(string hashedKey)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(hashedKey));
+        }
+    }
+}
