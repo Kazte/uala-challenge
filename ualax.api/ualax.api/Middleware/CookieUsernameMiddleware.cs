2a1ea24953fc0699ccc93cb59daf9074611b7f29
Kazte <castellittofranco1@gmail.com>
2025-02-28 00:12:07 -0300

    feat: middlewares

---
+++ ualax.api/ualax.api/Middleware/CookieUsernameMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using ualax.application.Abstractions.Authentication;
+
+namespace ualax.api.Middleware
+{
+    public class CookieUsernameMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IHasher _hasher;
+
+        public CookieUsernameMiddleware(RequestDelegate next, IHasher hasher)
+        {
+            _next = next;
+            _hasher = hasher;
+        }
+
+        public async Task InvokeAsnyc(HttpContext ctx)
+        {
+            if (ctx.Request.Cookies.TryGetValue("username", out var usernameBase64))
+            {
+                var username = _hasher.Unhash(usernameBase64);
+
+                ctx.Items["Username"] = username;
+            }
+
+            await _next(ctx);
+        }
+    }
+}
---
+++ ualax.api/ualax.api/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Text.Json;
+using ualax.application.Abstractions;
+using ualax.application.Abstractions.Exceptions;
+
+namespace ualax.api.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext ctx)
+        {
+            try
+            {
+                await _next(ctx);
+            }
+            catch (Exception ex)
+            {
+                var res = ctx.Response;
+                res.ContentType = "application/json";
+                var responseModel = new Response<string>
+                {
+                    IsSuccess = false,
+                    Message = ex?.Message
+                };
+
+                switch (ex)
+                {
+                    case ApiException e:
+                        res.StatusCode = e.StatusCode;
+                        break;
+
+                    case ValidationException e:
+                        res.StatusCode = (int)HttpStatusCode.BadRequest;
+                        responseModel.Errors = e.Errors.SelectMany(x => x.Value).ToList();
+                        break;
+
+                    default:
+                        res.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(responseModel);
+
+                await res.WriteAsync(result);
+            }
+        }
+    }
+}
