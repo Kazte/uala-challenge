52fc7e8aa74496f98473ee8e538d3a5933960cfa
Kazte <castellittofranco1@gmail.com>
2025-03-01 17:45:16 -0300

    feat: shared project

---
+++ Shared/Extension/DateTimeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Extension
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime? SetKindUtc(this DateTime? dateTime)
+        {
+            if (dateTime is null)
+            {
+                return null;
+            }
+
+            return dateTime.Value.SetKindUtc();
+        }
+
+        public static DateTime SetKindUtc(this DateTime dateTime)
+        {
+            if (dateTime.Kind == DateTimeKind.Utc)
+            {
+                return dateTime;
+            }
+
+            return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+        }
+    }
+}
