075ffbc09a052f6b1928436a16946fa80242a95a
Kazte <castellittofranco1@gmail.com>
2025-03-01 18:09:28 -0300

    refactor: add non data response

--- ualax.api/ualax.application/Abstractions/Response.cs
+++ ualax.api/ualax.application/Abstractions/Response.cs
@@ -2,34 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ualax.application.Abstractions
 {
-    public class Response<T>
+    public class Response
     {
+        [JsonPropertyName("isSuccess")]
         public bool IsSuccess { get; set; }
+        [JsonPropertyName("message")]
         public string Message { get; set; }
-        public List<string> Errors { get; set; }
-        public T Data { get; set; }
+        [JsonPropertyName("errors")]
+        public List<string>? Errors { get; set; }
+
+        public Response() { }
+
+        public Response(string message, bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+    }
+
+    public class Response<T> : Response
+    {
+        [JsonPropertyName("data")]
+        public T? Data { get; set; }
 
         public Response()
         {
-            
         }
 
-        public Response (T data, string message = null) {
-            IsSuccess = true;
-            Message = message;
+        public Response(T data, string message = null)
+            : base(message, true)
+        {
             Data = data;
         }
-
-        public Response(string message)
-        {
-            IsSuccess = false;
-            Message = message;
-        }
-
     }
 }
