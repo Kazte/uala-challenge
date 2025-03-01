b929b7f2ebcf58cd14bdbbea37e5dabe43b3c31f
Kazte <castellittofranco1@gmail.com>
2025-03-01 18:02:43 -0300

    feat: shared extensions

---
+++ ualax.api/ualax.shared/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace ualax.shared.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime? SetKindUtc(this DateTime? dateTime)
+        {
+            if (dateTime is null)
+            {
+                return null;
+            }
+
+            return dateTime.Value.SetKindUtc();
+        }
+
+        public static DateTime SetKindUtc(this DateTime dateTime)
+        {
+            if (dateTime.Kind == DateTimeKind.Utc)
+            {
+                return dateTime;
+            }
+
+            return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+        }
+    }
+}
