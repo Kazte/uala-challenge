cf36e42b8982dc65393b0bf19eab08f4ccedd896
Kazte <castellittofranco1@gmail.com>
2025-03-01 18:09:48 -0300

    feat: add follower repository

---
+++ ualax.api/ualax.domain/Features/Follow/IFollowRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ualax.domain.Features.Follow
+{
+    public interface IFollowRepository
+    {
+        Task<bool> IsFollowing(int followerId, int followedId);
+        Task Follow(FollowEntity follow);
+        Task<bool> Unfollow(FollowEntity follow);
+        Task<IEnumerable<int>> GetFollowersIds(int userId);
+    }
+}
